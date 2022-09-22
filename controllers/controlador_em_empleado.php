12d64753fbb15fb32bfddbce5864ea9deb822af5
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-22 14:17:29 -0500

    v0.61.9 Se integra funcion calcula controler base

--- controllers/controlador_em_empleado.php
+++ controllers/controlador_em_empleado.php
@@ -19,6 +19,7 @@ use html\em_empleado_html;
 use gamboamartin\empleado\models\em_empleado;
 use PDO;
 use stdClass;
+use Throwable;
 
 class controlador_em_empleado extends system {
 
@@ -224,4 +225,36 @@ class controlador_em_empleado extends system {
         }
         return $r_modifica_bd;
     }
+
+
+    public function calcula_sdi(bool $header, bool $ws = true){
+        $em_empleado_id = $_GET['em_empleado_id'];
+        $fecha_inicio_rel = $_GET['fecha_inicio_rel_laboral'];
+        $salario_diario = $_GET['salario_diario'];
+
+        $result = (new em_empleado($this->link))->calcula_sdi(em_empleado_id: $em_empleado_id,
+            fecha_inicio_rel: $fecha_inicio_rel, salario_diario: $salario_diario);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al obtener',data:  $result, header: $header,ws:$ws);
+        }
+
+        if($header){
+            $retorno = $_SERVER['HTTP_REFERER'];
+            header('Location:'.$retorno);
+            exit;
+        }
+        if($ws){
+            header('Content-Type: application/json');
+            try {
+                echo json_encode($result, JSON_THROW_ON_ERROR);
+            }
+            catch (Throwable $e){
+                return $this->retorno_error(mensaje: 'Error al maquetar estados',data:  $e, header: false,ws:$ws);
+            }
+            exit;
+        }
+
+        return $result;
+    }
+
 }

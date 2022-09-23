46776817299c729c2118ee7a5732a367c1d96e93
Kevin <kevin.acuna@ivitec.com.mx>
2022-09-23 17:38:59 -0500

    v.0.72.9 Se integran acciones para la generacion y listado de anticipos

--- templates/listas/em_empleado/row.php
+++ templates/listas/em_empleado/row.php
@@ -7,7 +7,8 @@
     <td><?php echo $row->em_empleado_nombre. ' '. $row->em_empleado_ap. ' '.$row->em_empleado_am; ?></td>
     <td><?php echo $row->em_empleado_rfc; ?></td>
 
-
+    <td><?php include 'templates/botons/em_empleado/link_genera_anticipo.php';?></td>
+    <td><?php include 'templates/botons/em_empleado/link_ver_anticipos.php';?></td>
     <!-- End dynamic generated -->
 
     <?php include (new views())->ruta_templates.'listas/action_row.php';?>
--- templates/listas/em_empleado/thead.php
+++ templates/listas/em_empleado/thead.php
@@ -6,6 +6,8 @@
         <th data-breakpoints="xs sm md"  data-type="html">Empleado</th>
         <th data-breakpoints="xs sm md"  data-type="html">RFC</th>
 
+        <th data-breakpoints="xs md" class="control"  data-type="html">Anticipos</th>
+        <th data-breakpoints="xs md" class="control"  data-type="html">Listar</th>
         <th data-breakpoints="xs md" class="control"  data-type="html">Modifica</th>
         <th data-breakpoints="xs md" class="control"  data-type="html">Elimina</th>
     </tr>

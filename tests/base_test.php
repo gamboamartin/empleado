a9aacfbb86c49c8d0ae35d7f4400e53133158826
mgamboa <gamboa.martin@gmail.com>
2022-10-03 15:33:52 -0500

    v0.118.0 Se integra em empleado test base

---
+++ tests/base_test.php
@@ -0,0 +1,44 @@
+<?php
+namespace gamboamartin\empleado\test;
+use base\orm\modelo_base;
+use gamboamartin\errores\errores;
+use gamboamartin\empleado\models\em_empleado;
+use PDO;
+
+class base_test{
+
+
+
+
+
+    public function alta_em_empleado(PDO $link): array|\stdClass
+    {
+        $registro = array();
+        $registro['id'] = 1;
+        $registro['codigo'] = 1;
+        $registro['descripcion'] = 1;
+
+
+
+        $alta = (new em_empleado($link))->alta_registro($registro);
+        if(errores::$error){
+            return (new errores())->error('Error al dar de alta ', $alta);
+
+        }
+        return $alta;
+    }
+
+
+
+
+    public function del(PDO $link, string $name_model): array
+    {
+        $model = (new modelo_base($link))->genera_modelo(modelo: $name_model);
+        $del = $model->elimina_todo();
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al eliminar '.$name_model, data: $del);
+        }
+        return $del;
+    }
+
+}

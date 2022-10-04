5f7a15ec123a68321f14d31f90ec1cdfac8e8a43
mgamboa <gamboa.martin@gmail.com>
2022-10-04 11:15:38 -0500

    v0.125.0 Se integra rfc in empleado

--- orm/em_empleado.php
+++ orm/em_empleado.php
@@ -1,5 +1,7 @@
 <?php
 namespace gamboamartin\empleado\models;
+
+
 use base\orm\modelo;
 use DateTime;
 use gamboamartin\cat_sat\models\cat_sat_regimen_fiscal;
@@ -13,10 +15,12 @@ use models\im_detalle_conf_prestaciones;
 use models\im_registro_patronal;
 use PDO;
 use stdClass;
+use Throwable;
 
 class em_empleado extends modelo{
-
+    public errores $error;
     public function __construct(PDO $link){
+        $this->error = new errores();
         $tabla = 'em_empleado';
 
         $columnas = array($tabla=>false, 'im_registro_patronal'=>$tabla, 'cat_sat_regimen_fiscal'=>$tabla,
@@ -39,18 +43,27 @@ class em_empleado extends modelo{
             'curp' => array('type' => 'inputs'), 'nss' => array('type' => 'inputs'),
             'salario_diario' => array('type' => 'inputs'), 'salario_diario_integrado' => array('type' => 'inputs'));
 
+        $tipo_campos = array();
+        $tipo_campos[] = 'rfc';
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas,campos_view: $campos_view);
+            columnas: $columnas,campos_view: $campos_view, tipo_campos: $tipo_campos);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
+    private function alias(array $registro): array
+    {
+        if(!isset($registro['alias'])){
+            $registro['alias'] = strtoupper($registro['descripcion']);
+        }
+        return $registro;
+    }
+
 
     public function alta_bd(): array|stdClass
     {
         $registro = $this->registro;
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al inicializar registro',data: $registro);
-        }
+
 
         $keys = array('nombre','ap');
         $valida = $this->validacion->valida_existencia_keys(keys: $keys, registro: $registro);
@@ -58,51 +71,14 @@ class em_empleado extends modelo{
             return $this->error->error(mensaje: 'Error al validar registro',data: $valida);
         }
 
-        if(!isset($registro['am'])){
-            $registro['am'] = '';
+        $registro = $this->init_alta(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar am',data: $registro);
         }
 
-        if (!isset($registro['descripcion'])) {
-            $registro['descripcion'] = $registro['nombre'].' '.$registro['ap'].' ';
-            $registro['descripcion'] .= $registro['am'];
-        }
 
-        $this->registro['descripcion'] = $registro['descripcion'];
+        $this->registro = $registro;
 
-        if(!isset($registro['alias'])){
-            $registro['alias'] = $registro['descripcion'];
-        }
-
-        $this->registro['alias'] = $registro['alias'];
-
-        if (!isset($registro['descripcion_select'])) {
-            $registro['descripcion_select'] = $registro['nombre'].' '.$registro['ap'].' ';
-            $registro['descripcion_select'] .= $registro['am'];
-        }
-
-        $this->registro['descripcion_select'] = $registro['descripcion_select'];
-
-        if (!isset($registro['codigo_bis'])) {
-            $registro['codigo_bis'] = $registro['codigo'];
-        }
-
-        $this->registro['codigo_bis'] = $registro['codigo_bis'];
-
-        if (!isset($registro['org_puesto_id'])) {
-            $registro['org_puesto_id'] =  (new org_puesto($this->link))->get_puesto_default_id();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener get_puesto_default_id',data: $registro['org_puesto_id']);
-            }
-            $this->registro['org_puesto_id'] = $registro['org_puesto_id'];
-        }
-
-        if (!isset($registro['cat_sat_tipo_jornada_nom_id'])) {
-            $cat_tipo_jornada_nom_id =  (new cat_sat_tipo_jornada_nom($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener cat_tipo_jornada_nom_id',data: $cat_tipo_jornada_nom_id);
-            }
-            $this->registro['cat_sat_tipo_jornada_nom_id'] = $cat_tipo_jornada_nom_id;
-        }
 
         $r_alta_bd = parent::alta_bd();
         if(errores::$error){
@@ -111,6 +87,133 @@ class em_empleado extends modelo{
 
         return $r_alta_bd;
 
+    }
+
+    /**
+     * Maqueta el apellido materno en vacio si no existe
+     * @param array $registro Registro en proceso
+     * @return array
+     * @version 0.125.0
+     */
+    private function am(array $registro): array
+    {
+        if(!isset($registro['am'])){
+            $registro['am'] = '';
+        }
+        return $registro;
+    }
+
+    public function calcula_sdi(int $em_empleado_id, string $fecha_inicio_rel, float $salario_diario): float|array
+    {
+        $factor = $this->obten_factor(em_empleado_id: $em_empleado_id, fecha_inicio_rel: $fecha_inicio_rel);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener factor',data:  $factor);
+        }
+
+        $sdi = $salario_diario * $factor;
+
+        return round($sdi,2);
+    }
+
+    private function cat_sat_tipo_jornada_nom_id(array $registro): array
+    {
+        if (!isset($registro['cat_sat_tipo_jornada_nom_id'])) {
+            $cat_tipo_jornada_nom_id =  (new cat_sat_tipo_jornada_nom($this->link))->id_predeterminado();
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al obtener cat_tipo_jornada_nom_id',data: $cat_tipo_jornada_nom_id);
+            }
+        }
+        return $registro;
+    }
+
+    private function codigo_bis(array $registro): array
+    {
+        if(!isset($registro['codigo_bis'])){
+            $registro['codigo_bis'] = strtoupper($registro['codigo']);
+        }
+        return $registro;
+    }
+
+    private function descripcion(array $registro): array
+    {
+        if (!isset($registro['descripcion'])) {
+            $registro['descripcion'] = $registro['nombre'].' '.$registro['ap'].' ';
+            $registro['descripcion'] .= $registro['am'];
+        }
+        return $registro;
+    }
+
+    private function descripcion_select(array $registro): array
+    {
+        if (!isset($registro['descripcion_select'])) {
+            $registro['descripcion_select'] = $registro['rfc'];
+            $registro['descripcion_select'] .= $registro['nombre'].' '.$registro['ap'].' ';
+            $registro['descripcion_select'] .= $registro['am'];
+            $registro['descripcion_select'] = strtoupper($registro['descripcion_select']);
+        }
+        return $registro;
+    }
+
+    /**
+     * Obtiene empresa a partir de empleado
+     * @param int $em_empleado_id Identificador del empleado a revisar su empresa
+     * @return array|stdClass
+     * @version
+     */
+    public function get_empresa(int $em_empleado_id): array|stdClass
+    {
+        $r_empleado = $this->registro(registro_id: $em_empleado_id);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener empleado',data: $r_empleado);
+        }
+
+        $r_registro_patronal =  (new im_registro_patronal($this->link))->registro(registro_id:
+            $r_empleado['im_registro_patronal_id']);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener registro patronal',data: $r_registro_patronal);
+        }
+
+        return $r_registro_patronal;
+    }
+
+    private function init_alta(array $registro): array
+    {
+        $registro = $this->am(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar am',data: $registro);
+        }
+
+
+        $registro = $this->descripcion(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar descripcion',data: $registro);
+        }
+
+        $registro = $this->alias(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar alias',data: $registro);
+        }
+
+        $registro = $this->descripcion_select(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar descripcion',data: $registro);
+        }
+
+        $registro = $this->codigo_bis(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar codigo_bis',data: $registro);
+        }
+
+        $registro = $this->org_puesto_id(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar org puesto',data: $registro);
+        }
+        $registro = $this->cat_sat_tipo_jornada_nom_id(registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar cat_sat_tipo_jornada_nom_id',data: $registro);
+        }
+
+        return $registro;
     }
 
     public function modifica_bd(array $registro, int $id, bool $reactiva = false): array|stdClass
@@ -137,28 +240,10 @@ class em_empleado extends modelo{
 
     }
 
-    /**
-     * Obtiene empresa a partir de empleado
-     * @param int $em_empleado_id Identificador del empleado a revisar su empresa
-     * @return array|stdClass
-     * @version
-     */
-    public function get_empresa(int $em_empleado_id){
-        $r_empleado = $this->registro(registro_id: $em_empleado_id);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener empleado',data: $r_empleado);
-        }
 
-        $r_registro_patronal =  (new im_registro_patronal($this->link))->registro(registro_id:
-            $r_empleado['im_registro_patronal_id']);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener registro patronal',data: $r_registro_patronal);
-        }
 
-        return $r_registro_patronal;
-    }
-
-    public function obten_conf(int $em_empleado_id){
+    public function obten_conf(int $em_empleado_id): array
+    {
         $imss_modelo = new im_conf_pres_empresa($this->link);
         $empresa = $imss_modelo->obten_configuraciones_empresa(em_empleado_id: $em_empleado_id);
         if(errores::$error){
@@ -198,7 +283,8 @@ class em_empleado extends modelo{
         return $datos_sdi;
     }
 
-    public function obten_factor(int $em_empleado_id, string $fecha_inicio_rel){
+    public function obten_factor(int $em_empleado_id, string $fecha_inicio_rel): float|array
+    {
         $detalle = $this->obten_detalle(em_empleado_id: $em_empleado_id, fecha_inicio_rel: $fecha_inicio_rel);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al obtener detalle',data:  $detalle);
@@ -212,20 +298,9 @@ class em_empleado extends modelo{
         return round($dias_sdi/365, 4);
     }
 
-    public function calcula_sdi(int $em_empleado_id, string $fecha_inicio_rel, float $salario_diario){
-        $factor = $this->obten_factor(em_empleado_id: $em_empleado_id, fecha_inicio_rel: $fecha_inicio_rel);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener factor',data:  $factor);
-        }
 
-        $sdi = $salario_diario * $factor;
 
-        return round($sdi,2);
-    }
 
-    /**
-     * @throws \Exception
-     */
     private function obten_years(string $fecha_calculo, string $fecha_inicio_rel): int|array
     {
         $fecha_calculo = trim($fecha_calculo);
@@ -252,10 +327,27 @@ class em_empleado extends modelo{
             calculada",$fecha_calculo);
         }
 
-        $date1 = new DateTime($fecha_inicio_rel);
-        $date2 = new DateTime($fecha_calculo);
-        $diff = $date1->diff($date2);
+        try {
+            $date1 = new DateTime($fecha_inicio_rel);
+            $date2 = new DateTime($fecha_calculo);
+            $diff = $date1->diff($date2);
+        }
+        catch (Throwable $e){
+            return $this->error->error(mensaje:"Error al calcular fecha",data: $e);
+        }
+
 
         return $diff->y;
+    }
+
+    private function org_puesto_id(array $registro): array
+    {
+        if (!isset($registro['org_puesto_id'])) {
+            $registro['org_puesto_id'] =  (new org_puesto($this->link))->get_puesto_default_id();
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al obtener get_puesto_default_id',data: $registro['org_puesto_id']);
+            }
+        }
+        return $registro;
     }
 }
---
+++ tests/orm/em_empleadoTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace tests\templates\directivas;
+
+use gamboamartin\empleado\models\em_empleado;
+use gamboamartin\errores\errores;
+use gamboamartin\test\liberator;
+use gamboamartin\test\test;
+use stdClass;
+
+
+class em_empleadoTest extends test {
+    public errores $errores;
+    private stdClass $paths_conf;
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->errores = new errores();
+        $this->paths_conf = new stdClass();
+        $this->paths_conf->generales = '/var/www/html/cat_sat/config/generales.php';
+        $this->paths_conf->database = '/var/www/html/cat_sat/config/database.php';
+        $this->paths_conf->views = '/var/www/html/cat_sat/config/views.php';
+    }
+
+    public function test_am(): void
+    {
+        errores::$error = false;
+
+        $_GET['seccion'] = 'cat_sat_tipo_persona';
+        $_GET['accion'] = 'lista';
+        $_SESSION['grupo_id'] = 1;
+        $_SESSION['usuario_id'] = 2;
+        $_GET['session_id'] = '1';
+
+        $modelo = new em_empleado($this->link);
+        $modelo = new liberator($modelo);
+
+        //$html = new liberator($html);
+        $registro = array();
+        $resultado = $modelo->am($registro);
+
+        $this->assertIsArray($resultado);
+        $this->assertNotTrue(errores::$error);
+        $this->assertEquals("", $resultado['am']);
+
+        errores::$error = false;
+    }
+
+
+
+
+
+
+}
+

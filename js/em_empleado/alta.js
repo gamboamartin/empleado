a5a8c9ac26ebcc9f316a503714d4eb83221709fa
Kevin <kevin.acuna@ivitec.com.mx>
2022-10-26 09:54:49 -0500

    v0.165 Se integra input automatico para direcciones

--- js/em_empleado/alta.js
+++ js/em_empleado/alta.js
@@ -6,54 +6,51 @@ let sl_dp_colonia = $("#dp_colonia_id");
 let sl_dp_colonia_postal = $("#dp_colonia_postal_id");
 let sl_dp_calle = $("#dp_calle_id");
 let sl_dp_calle_pertenece = $("#dp_calle_pertenece_id");
+
+let sl_org_puesto = $("#org_puesto_id");
+
+
 let in_campo_extra = $("#campo_extra");
 
 let input_seleccionado;
 
-let sl_predeterminado = (identificador) => {
+$(function() {
+    var content = "<input type='text' class='bss-input' onKeyDown='event.stopPropagation();' onKeyPress='addSelectInpKeyPress(this,event)' onClick='event.stopPropagation()' placeholder='Add item'> <span class='glyphicon glyphicon-plus addnewicon' onClick='addSelectItem(this,event,1);'></span>";
 
-    identificador.change(function(){
-        let selected = identificador.find('option:selected');
-        let objeto = identificador.attr('id');
-        objeto = objeto.substring(0, objeto.length - 2);
-        let predeterminado = selected.data(`${objeto}predeterminado`);
-        input_seleccionado = null;
 
-        if (predeterminado === 'activo'){
-            let campo = identificador.parent().parent().siblings().text()
-            in_campo_extra.parent().siblings().text(campo)
-            input_seleccionado = identificador;
-            in_campo_extra.val("")
-            $('#campo').modal('show');
-        }
-    });
-};
+    var addoption = $('<option/>', {class: 'addItem'})
+        .data('content', content)
 
-$("#modal-aceptar").click(function () {
-    if (input_seleccionado != null){
-        let name = input_seleccionado.attr('name');
-        let value = in_campo_extra.val();
-        let id = `#campo_extra_${name}`;
-        console.log(id)
-        console.log($(id).length)
+    let sl_predeterminado = (identificador) => {
 
-        if($(id).length <= 0){
-            let input = jQuery(`<input type="hidden" id="campo_extra_${name}" name="campo_extra_${name}" value="${value}">`);
-            jQuery('.form-additional').append(input);
-        }else{
-            $(id).val(value)
-        }
-    }
+        identificador.change(function(){
+            let selected = identificador.find('option:selected');
+            let id = identificador.attr('id');
+            let objeto = id.substring(0, id.length - 2);
+            let predeterminado = selected.data(`${objeto}predeterminado`);
+
+            if (predeterminado === 'activo'){
+                $(`#${id}`).siblings(".dropdown-menu").children(".inner").append('<li data-original-index="198"><a tabindex="0" class="addItem"  data-tokens="null" style="display: flex;justify-content: space-evenly;align-items: center;" ><input type="text" class="bss-input" style="width: 90%;" onkeydown="event.stopPropagation();" onkeypress="addSelectInpKeyPress(this,event)" onclick="event.stopPropagation()" placeholder="Add item"> <span class="glyphicon glyphicon-plus addnewicon" onclick="addSelectItem(this,event,1);"></span><span class="glyphicon glyphicon-ok check-mark"></span></a></li>');
+                $(`#${id}`).prepend(addoption)
+            }
+        });
+    };
+
+    sl_predeterminado(sl_dp_pais);
+    sl_predeterminado(sl_dp_estado);
+    sl_predeterminado(sl_dp_municipio);
+    sl_predeterminado(sl_dp_cp);
+    sl_predeterminado(sl_dp_colonia);
+    sl_predeterminado(sl_dp_colonia_postal);
+    sl_predeterminado(sl_dp_calle);
+    sl_predeterminado(sl_dp_calle_pertenece);
+
+
+
+
 });
 
-sl_predeterminado(sl_dp_pais);
-sl_predeterminado(sl_dp_estado);
-sl_predeterminado(sl_dp_municipio);
-sl_predeterminado(sl_dp_cp);
-sl_predeterminado(sl_dp_colonia);
-sl_predeterminado(sl_dp_colonia_postal);
-sl_predeterminado(sl_dp_calle);
-sl_predeterminado(sl_dp_calle_pertenece);
+
 
 
 

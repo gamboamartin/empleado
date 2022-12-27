c530885eb76384d11420da1eccc13ced588b3e3c
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-12-27 15:23:08 -0600

    v0.191.7 se ajusta vista de tipo_ant

--- controllers/controlador_em_tipo_anticipo.php
+++ controllers/controlador_em_tipo_anticipo.php
@@ -9,6 +9,7 @@
 namespace gamboamartin\empleado\controllers;
 
 use gamboamartin\empleado\models\em_tipo_anticipo;
+use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
 use gamboamartin\template\html;
@@ -18,14 +19,92 @@ use stdClass;
 
 class controlador_em_tipo_anticipo extends system {
 
+    public array $keys_selects = array();
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new em_tipo_anticipo(link: $link);
         $html_ = new em_tipo_anticipo_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
-        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $columns["em_tipo_anticipo_id"]["titulo"] = "Id";
+        $columns["em_tipo_anticipo_codigo"]["titulo"] = "Código";
+        $columns["em_tipo_anticipo_descripcion"]["titulo"] = "Tipo";
+
+        $filtro = array("em_tipo_anticipo.id","em_tipo_anticipo.codigo","em_tipo_anticipo.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipo Anticipo';
+
+        $propiedades = $this->inicializa_propiedades();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+            print_r($error);
+            die('Error');
+        }
+    }
+
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta =  parent::alta(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_alta;
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
+    }
+
+    private function inicializa_propiedades(): array
+    {
+        $identificador = "codigo";
+        $propiedades = array("place_holder" => "Código", "cols" => 4);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "descripcion";
+        $propiedades = array("place_holder" => "Tipo", "cols" => 8);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        return $this->keys_selects;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+        $r_modifica =  parent::modifica(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_modifica;
     }
 
 }
--- orm/em_tipo_anticipo.php
+++ orm/em_tipo_anticipo.php
@@ -9,10 +9,13 @@ class em_tipo_anticipo extends modelo{
     public function __construct(PDO $link){
         $tabla = 'em_tipo_anticipo';
         $columnas = array($tabla=>false);
-        $campos_obligatorios = array('descripcion','codigo','descripcion_select','alias','codigo_bis');
+        $campos_obligatorios = array('descripcion');
+
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['descripcion'] = array('type' => 'inputs');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, campos_view: $campos_view);
 
         $this->NAMESPACE = __NAMESPACE__;
     }

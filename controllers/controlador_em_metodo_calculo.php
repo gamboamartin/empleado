8a80cfe4c0d9bcfe8076aeb75606ba33c8a5fd47
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-12-30 13:59:38 -0600

    v1.5.0 Se ajusta vista metodo

--- controllers/controlador_em_metodo_calculo.php
+++ controllers/controlador_em_metodo_calculo.php
@@ -19,14 +19,58 @@ use stdClass;
 
 class controlador_em_metodo_calculo extends system {
 
+    public array $keys_selects = array();
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new em_metodo_calculo(link: $link);
         $html_ = new em_metodo_calculo_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
-        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $columns["em_metodo_calculo_id"]["titulo"] = "Id";
+        $columns["em_metodo_calculo_codigo"]["titulo"] = "Código";
+        $columns["em_metodo_calculo_descripcion"]["titulo"] = "Metodo";
+
+        $filtro = array("em_metodo_calculo.id","em_metodo_calculo.codigo","em_metodo_calculo.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipo Descuento';
+
+        $propiedades = $this->inicializa_propiedades();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+            print_r($error);
+            die('Error');
+        }
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
+    }
+
+    private function inicializa_propiedades(): array
+    {
+        $identificador = "codigo";
+        $propiedades = array("place_holder" => "Código", "cols" => 4);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "descripcion";
+        $propiedades = array("place_holder" => "Metodo", "cols" => 8);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        return $this->keys_selects;
     }
 
     public function alta(bool $header, bool $ws = false): array|string
@@ -36,46 +80,33 @@ class controlador_em_metodo_calculo extends system {
             return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
         }
 
-        $this->row_upd->monto = 0;
-
-        $inputs = $this->genera_inputs();
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
         if(errores::$error){
             $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
             print_r($error);
             die('Error');
         }
+
         return $r_alta;
     }
 
-    private function base(): array|stdClass
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
     {
         $r_modifica =  parent::modifica(header: false);
         if(errores::$error){
-            return $this->errores->error(mensaje: 'Error al generar template',data:  $r_modifica);
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
         }
 
-        $inputs = $this->genera_inputs();
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
         if(errores::$error){
-            return $this->errores->error(mensaje: 'Error al inicializar inputs',data:  $inputs);
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
         }
 
-        $data = new stdClass();
-        $data->template = $r_modifica;
-        $data->inputs = $inputs;
-
-        return $data;
+        return $r_modifica;
     }
 
-    public function modifica(bool $header, bool $ws = false, string $breadcrumbs = '', bool $aplica_form = true,
-                             bool $muestra_btn = true): array|stdClass
-    {
-        $base = $this->base();
-        if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al maquetar datos',data:  $base,
-                header: $header,ws:$ws);
-        }
-
-        return $base->template;
-    }
 
 }
--- orm/em_metodo_calculo.php
+++ orm/em_metodo_calculo.php
@@ -9,10 +9,13 @@ class em_metodo_calculo extends modelo{
     public function __construct(PDO $link){
         $tabla = 'em_metodo_calculo';
         $columnas = array($tabla=>false);
-        $campos_obligatorios = array('descripcion','codigo','descripcion_select','alias','codigo_bis');
+        $campos_obligatorios = array('descripcion','codigo');
+
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['descripcion'] = array('type' => 'inputs');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas,campos_view: $campos_view);
 
         $this->NAMESPACE = __NAMESPACE__;
     }

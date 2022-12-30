3c403a637b3cdd7b128ea3b0386d7ecc589f4a92
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-12-30 13:38:14 -0600

    v1.4.0 Se ajusta entidad metodo calculo

--- controllers/controlador_em_metodo_calculo.php
+++ controllers/controlador_em_metodo_calculo.php
@@ -8,21 +8,21 @@
  */
 namespace gamboamartin\empleado\controllers;
 
-use gamboamartin\empleado\models\em_tipo_descuento;
+use gamboamartin\empleado\models\em_metodo_calculo;
 use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
 use gamboamartin\template\html;
-use html\em_tipo_anticipo_html;
+use html\em_metodo_calculo_html;
 use PDO;
 use stdClass;
 
-class controlador_em_tipo_descuento extends system {
+class controlador_em_metodo_calculo extends system {
 
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
-        $modelo = new em_tipo_descuento(link: $link);
-        $html_ = new em_tipo_anticipo_html(html: $html);
+        $modelo = new em_metodo_calculo(link: $link);
+        $html_ = new em_metodo_calculo_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
         parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 

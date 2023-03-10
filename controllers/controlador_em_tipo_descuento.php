ac9c659030d14274c504f087977a5b9046a16d13
Kevin <kevin.acuna@ivitec.com.mx>
2023-03-10 12:46:01 -0500

    v2.27.8 Se ajusta acciones en tipo descuento

--- controllers/controlador_em_tipo_descuento.php
+++ controllers/controlador_em_tipo_descuento.php
@@ -6,120 +6,172 @@
  * @final En proceso
  *
  */
+
 namespace gamboamartin\empleado\controllers;
 
+use base\controller\controler;
 use gamboamartin\empleado\models\em_tipo_descuento;
 use gamboamartin\errores\errores;
+use gamboamartin\system\_ctl_base;
 use gamboamartin\system\links_menu;
-use gamboamartin\system\system;
 use gamboamartin\template\html;
 use html\em_tipo_anticipo_html;
 use PDO;
 use stdClass;
 
-class controlador_em_tipo_descuento extends system {
+class controlador_em_tipo_descuento extends _ctl_base
+{
 
-    public array|stdClass $keys_selects = array();
-    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
-                                stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO      $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass())
+    {
         $modelo = new em_tipo_descuento(link: $link);
         $html_ = new em_tipo_anticipo_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
 
-        $columns["em_tipo_descuento_id"]["titulo"] = "Id";
-        $columns["em_tipo_descuento_codigo"]["titulo"] = "Código";
-        $columns["em_tipo_descuento_descripcion"]["titulo"] = "Tipo Descuento";
-        $columns["em_metodo_calculo_descripcion"]["titulo"] = "Metodo Calculo";
-        $columns["em_tipo_descuento_monto"]["titulo"] = "Monto";
-
-        $filtro = array("em_tipo_descuento.id","em_tipo_descuento.codigo","em_tipo_descuento.descripcion");
-
-        $datatables = new stdClass();
-        $datatables->columns = $columns;
-        $datatables->filtro = $filtro;
-
-        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link,datatables: $datatables,
-            paths_conf: $paths_conf);
-
-        $this->titulo_lista = 'Tipo Descuento';
-        $propiedades = $this->inicializa_propiedades();
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+        $datatables = $this->init_datatable();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable', data: $datatables);
             print_r($error);
             die('Error');
         }
 
-        $this->lista_get_data = true;
-    }
+        parent::__construct(html: $html_, link: $link, modelo: $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
 
-    public function asignar_propiedad(string $identificador, array $propiedades): array|stdClass
-    {
-        $identificador = trim($identificador);
-        if($identificador === ''){
-            return $this->errores->error(mensaje: 'Error identificador esta vacio',data:  $identificador);
+        $configuraciones = $this->init_configuraciones();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar configuraciones', data: $configuraciones);
+            print_r($error);
+            die('Error');
         }
-
-        if (!array_key_exists($identificador,$this->keys_selects)){
-            $this->keys_selects[$identificador] = new stdClass();
-        }
-
-        foreach ($propiedades as $key => $value){
-            $this->keys_selects[$identificador]->$key = $value;
-        }
-        return $this->keys_selects;
-    }
-
-    private function inicializa_propiedades(): array
-    {
-        $identificador = "codigo";
-        $propiedades = array("place_holder" => "Código", "cols" => 4);
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
-
-        $identificador = "descripcion";
-        $propiedades = array("place_holder" => "Tipo", "cols" => 8);
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
-
-        $identificador = "em_metodo_calculo_id";
-        $propiedades = array("place_holder" => "Metodo Calculo");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
-
-        return $this->keys_selects;
     }
 
     public function alta(bool $header, bool $ws = false): array|string
     {
-        $r_alta =  parent::alta(header: false);
-        if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        $r_alta = $this->init_alta();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar alta', data: $r_alta, header: $header, ws: $ws);
         }
 
-        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
-            print_r($error);
-            die('Error');
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
+                ws: $ws);
+        }
+
+        $inputs = $this->inputs(keys_selects: $keys_selects);
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al obtener inputs', data: $inputs, header: $header, ws: $ws);
         }
 
         return $r_alta;
     }
 
+    protected function campos_view(): array
+    {
+        $keys = new stdClass();
+        $keys->inputs = array('codigo', 'descripcion', 'monto');
+        $keys->selects = array();
+
+        $init_data = array();
+        $init_data['em_metodo_calculo'] = "gamboamartin\\empleado";
+
+        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
+        }
+
+        return $campos_view;
+    }
+
+    private function init_configuraciones(): controler
+    {
+        $this->seccion_titulo = 'Tipo Descuento';
+        $this->titulo_lista = 'Registro de Tipos de Descuentos';
+
+        $this->lista_get_data = true;
+
+        return $this;
+    }
+
+    private function init_datatable(): stdClass
+    {
+        $columns["em_tipo_descuento_id"]["titulo"] = "Id";
+        $columns["em_metodo_calculo_descripcion"]["titulo"] = "Método Cálculo";
+        $columns["em_tipo_descuento_monto"]["titulo"] = "Monto";
+
+        $filtro = array("em_tipo_descuento.id", "em_metodo_calculo.descripcion", "em_tipo_descuento.monto");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        return $datatables;
+    }
+
+    private function init_selects(array $keys_selects, string $key, string $label, int $id_selected = -1, int $cols = 6,
+                                  bool  $con_registros = true, array $filtro = array()): array
+    {
+        $keys_selects = $this->key_select(cols: $cols, con_registros: $con_registros, filtro: $filtro, key: $key,
+            keys_selects: $keys_selects, id_selected: $id_selected, label: $label);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        return $keys_selects;
+    }
+
+    public function init_selects_inputs(): array
+    {
+        return $this->init_selects(keys_selects: array(), key: "em_metodo_calculo_id", label: "Método Cálculo");
+    }
+
+    protected function key_selects_txt(array $keys_selects): array
+    {
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'codigo',
+            keys_selects: $keys_selects, place_holder: 'Código');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 12, key: 'descripcion',
+            keys_selects: $keys_selects, place_holder: 'Descripción');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'monto',
+            keys_selects: $keys_selects, place_holder: 'Monto');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        return $keys_selects;
+    }
 
     public function modifica(bool $header, bool $ws = false): array|stdClass
     {
-        $r_modifica =  parent::modifica(header: false);
-        if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
+        $r_modifica = $this->init_modifica();
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al generar salida de template', data: $r_modifica, header: $header, ws: $ws);
         }
 
-        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
-            print_r($error);
-            die('Error');
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
+                ws: $ws);
+        }
+
+        $keys_selects['em_metodo_calculo_id']->id_selected = $this->registro['em_metodo_calculo_id'];
+
+        $base = $this->base_upd(keys_selects: $keys_selects, params: array(), params_ajustados: array());
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
         }
 
         return $r_modifica;
     }
-
-
 }
--- templates/inputs/em_tipo_descuento/alta.php
+++ templates/inputs/em_tipo_descuento/alta.php
@@ -1,7 +1,5 @@
 <?php /** @var  \gamboamartin\empleado\controllers\controlador_em_tipo_descuento $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->inputs->codigo; ?>
-<?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->em_metodo_calculo_id; ?>
 <?php echo $controlador->inputs->monto; ?>
-<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>

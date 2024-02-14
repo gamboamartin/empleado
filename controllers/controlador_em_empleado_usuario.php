09403871d4d7eb76733bb4228b9a612e59e08af3
Kevin <kevin.acuna@ivitec.com.mx>
2024-02-14 14:20:50 -0500

    v6.2.0 Se integra campo dependiente

--- controllers/controlador_em_empleado_usuario.php
+++ controllers/controlador_em_empleado_usuario.php
@@ -83,7 +83,7 @@ class controlador_em_empleado_usuario extends _ctl_base {
 
         $init_data = array();
         $init_data['em_empleado'] = "gamboamartin\\empleado";
-
+        $init_data['adm_usuario'] = "gamboamartin\\administrador";
 
         $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
         if (errores::$error) {
@@ -120,10 +120,10 @@ class controlador_em_empleado_usuario extends _ctl_base {
     }
 
     private function init_selects(array $keys_selects, string $key, string $label, int $id_selected = -1, int $cols = 6,
-                                  bool  $con_registros = true, array $filtro = array()): array
+                                  bool  $con_registros = true, array $filtro = array(), array $columns_ds = array()): array
     {
         $keys_selects = $this->key_select(cols: $cols, con_registros: $con_registros, filtro: $filtro, key: $key,
-            keys_selects: $keys_selects, id_selected: $id_selected, label: $label);
+            keys_selects: $keys_selects, id_selected: $id_selected, label: $label,columns_ds: $columns_ds);
         if (errores::$error) {
             return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
         }
@@ -133,7 +133,9 @@ class controlador_em_empleado_usuario extends _ctl_base {
 
     public function init_selects_inputs(): array
     {
-        return $this->init_selects(keys_selects: array(), key: "em_empleado_id", label: "Empleado", cols: 8);
+        $keys_selects = $this->init_selects(keys_selects: array(), key: "em_empleado_id", label: "Empleado", cols: 12);
+        return $this->init_selects(keys_selects: $keys_selects, key: "adm_usuario_id", label: "Usuario", cols: 12,
+            columns_ds: array("adm_usuario_user"));
     }
 
     protected function key_selects_txt(array $keys_selects): array

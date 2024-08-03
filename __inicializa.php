cb8a3e4e4fde7f42224dc4860bae527d877d2578
ivitec <ivi.tec1983@gmail.com>
2024-08-02 22:58:52 -0600

    v14.18.0 Se agregan campos entidades en instalacion

--- __inicializa.php
+++ __inicializa.php
@@ -11,120 +11,118 @@ require 'vendor/autoload.php';
 $con = new conexion();
 $link = conexion::$link;
 
-$link->beginTransaction();
 $administrador = new gamboamartin\administrador\instalacion\instalacion();
 
 $instala = $administrador->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar administrador', data: $administrador);
     print_r($error);
     exit;
 }
 
 
-print_r($instala);
+$dir_postal = new gamboamartin\direccion_postal\instalacion\instalacion();
 
+$instala = $dir_postal->instala(link: $link);
+if(errores::$error){
 
-$cat_sat = new gamboamartin\cat_sat\instalacion\instalacion();
+    $error = (new errores())->error(mensaje: 'Error al instalar $dir_postal', data: $instala);
+    print_r($error);
+    exit;
+}
+
+$cat_sat = new gamboamartin\cat_sat\instalacion\instalacion(link: $link);
 
 $instala = $cat_sat->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar cat_sat', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
+
 
 $proceso = new gamboamartin\proceso\instalacion\instalacion();
 
 $instala = $proceso->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar proceso', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($proceso);
 
 
 $documento = new gamboamartin\documento\instalacion\instalacion();
 
 $instala = $documento->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar documento', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
 
 $notificacion = new gamboamartin\notificaciones\instalacion\instalacion();
 
 $instala = $notificacion->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar notificacion', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
 
 $organigrama = new gamboamartin\organigrama\instalacion\instalacion();
 
 $instala = $organigrama->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar organigrama', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
 
 
 $comercial = new gamboamartin\comercial\instalacion\instalacion();
 
 $instala = $comercial->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar comercial', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
 
 
 $facturacion = new gamboamartin\facturacion\instalacion\instalacion();
 
 $instala = $facturacion->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar facturacion', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
 
 $empleado = new gamboamartin\empleado\instalacion\instalacion();
 
 $instala = $empleado->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+
     $error = (new errores())->error(mensaje: 'Error al instalar empleado', data: $instala);
     print_r($error);
     exit;
 }
 
-print_r($instala);
 
-$link->commit();
 
 
--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -60,6 +60,227 @@ class instalacion
         return $out;
     }
 
+    private function _add_em_tipo_abono_anticipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'em_tipo_abono_anticipo');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+
+
+        return $out;
+    }
+
+    private function _add_em_tipo_anticipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'em_tipo_anticipo');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+
+
+        return $out;
+    }
+    private function _add_em_clase_riesgo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'em_clase_riesgo');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $columnas->factor = new stdClass();
+        $columnas->factor->tipo_dato = 'DOUBLE';
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  'em_clase_riesgo');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+
+
+        return $out;
+    }
+
+
+    private function _add_em_anticipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'em_anticipo');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $foraneas = array();
+        $foraneas['em_tipo_anticipo_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  'em_anticipo');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+
+        $out->foraneas = $result;
+
+
+
+        return $out;
+    }
+
+
+    private function em_tipo_abono_anticipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+
+        $create = $this->_add_em_tipo_abono_anticipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+
+        $out->create = $create;
+
+        $adm_menu_descripcion = 'Abonos';
+        $adm_sistema_descripcion = 'empleado';
+        $etiqueta_label = 'Tipos de Abonos Anticipos';
+        $adm_seccion_pertenece_descripcion = 'em_tipo_abono_anticipo';
+        $adm_namespace_name = 'gamboamartin/empleado';
+        $adm_namespace_descripcion = 'gamboa.martin/empleado';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__,
+            adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion,
+            etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+
+        return $out;
+
+    }
+
+    private function em_tipo_anticipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+
+        $create = $this->_add_em_tipo_anticipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+
+        $out->create = $create;
+
+        $adm_menu_descripcion = 'Anticipos';
+        $adm_sistema_descripcion = 'empleado';
+        $etiqueta_label = 'Tipos de Anticipos';
+        $adm_seccion_pertenece_descripcion = 'em_tipo_anticipo';
+        $adm_namespace_name = 'gamboamartin/empleado';
+        $adm_namespace_descripcion = 'gamboa.martin/empleado';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__,
+            adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion,
+            etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+
+        return $out;
+
+    }
+
+    private function em_clase_riesgo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+
+        $create = $this->_add_em_clase_riesgo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+
+        $out->create = $create;
+
+        $adm_menu_descripcion = 'IMSS';
+        $adm_sistema_descripcion = 'empleado';
+        $etiqueta_label = 'Clases de Riesgo';
+        $adm_seccion_pertenece_descripcion = 'em_clase_riesgo';
+        $adm_namespace_name = 'gamboamartin/empleado';
+        $adm_namespace_descripcion = 'gamboa.martin/empleado';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__,
+            adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion,
+            etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+
+        return $out;
+
+    }
+
+    private function em_anticipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+
+        $create = $this->_add_em_anticipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+
+        $out->create = $create;
+
+        $adm_menu_descripcion = 'Anticipos';
+        $adm_sistema_descripcion = 'empleado';
+        $etiqueta_label = 'Anticipos';
+        $adm_seccion_pertenece_descripcion = 'em_anticipo';
+        $adm_namespace_name = 'gamboamartin/empleado';
+        $adm_namespace_descripcion = 'gamboa.martin/empleado';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__,
+            adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion,
+            etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+
+        return $out;
+
+    }
 
 
     private function em_abono_anticipo(PDO $link): array|stdClass
@@ -99,6 +320,30 @@ class instalacion
     final public function instala(PDO $link): array|stdClass
     {
         $out = new stdClass();
+
+        $em_abono_tipo_anticipo = $this->em_tipo_abono_anticipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar $em_abono_tipo_anticipo', data:  $em_abono_tipo_anticipo);
+        }
+        $out->em_abono_tipo_anticipo = $em_abono_tipo_anticipo;
+
+        $em_tipo_anticipo = $this->em_tipo_anticipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar em_tipo_anticipo', data:  $em_tipo_anticipo);
+        }
+        $out->em_abono_tipo_anticipo = $em_abono_tipo_anticipo;
+
+        $em_clase_riesgo = $this->em_clase_riesgo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar em_clase_riesgo', data:  $em_clase_riesgo);
+        }
+        $out->em_clase_riesgo = $em_clase_riesgo;
+
+        $em_anticipo = $this->em_anticipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar em_anticipo', data:  $em_anticipo);
+        }
+        $out->em_anticipo = $em_anticipo;
 
         $em_abono_anticipo = $this->em_abono_anticipo(link: $link);
         if(errores::$error){
